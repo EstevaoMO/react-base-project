a14d9e1c64bc75926e953bf46cc81462291cb2b7
ESTEVAO BATISTA DE MORAES <202402070134@alunos.ibmec.edu.br>
2024-10-03 17:08:37 -0300

    commit

---
+++ src/pages/Nova.jsx
@@ -0,0 +1,15 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+import Base from "./Base"
+
+const Nova = () => {
+    return (
+        <Base>
+          <p>ooiii</p>
+          <h1>Nova Página</h1><br/>
+          <NovoComponente/>
+        </Base>
+      )
+};
+
+
+export default Nova;
